7fd3fbe5b89b41617c28e2729ee75ba94860ed7b
baieric <ebai@uwaterloo.ca>
2016-01-23 16:34:48 -0500

    pedometer

--- app/src/main/java/transcendentlabs/com/cloudwalk/MainActivity.java
+++ app/src/main/java/transcendentlabs/com/cloudwalk/MainActivity.java
@@ -20,22 +20,6 @@ public class MainActivity extends ActionBarActivity {
 
         setContentView(R.layout.activity_main);
 
-// Add your initialization code here
-
-        Parse.initialize(this, BuildConfig.PARSE_APPLICATION_ID, BuildConfig.PARSE_CLIENT_KEY);
-
-        ParseUser.enableAutomaticUser();
-
-        ParseACL defaultACL = new ParseACL();
-
-// If you would like all objects to be private by default, remove this
-
-// line.
-
-        defaultACL.setPublicReadAccess(true);
-
-        ParseACL.setDefaultACL(defaultACL, true);
-
 // Determine whether the current user is an anonymous user
         if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
             // If user is anonymous, send the user to LoginSignupActivity.class
--- app/src/main/java/transcendentlabs/com/cloudwalk/WelcomeActivity.java
+++ app/src/main/java/transcendentlabs/com/cloudwalk/WelcomeActivity.java
@@ -2,25 +2,37 @@ package transcendentlabs.com.cloudwalk;
 import com.parse.ParseUser;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class WelcomeActivity extends Activity {
+public class WelcomeActivity extends Activity implements SensorEventListener {
 
+    private final String TOTAL_STEPS = "totalSteps";
     // Declare Variable
     Button logout;
+    private SensorManager sensorManager;
+    private TextView count;
+    boolean activityRunning;
+    ParseUser currentUser;
+    int totalSteps;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Get the view from singleitemview.xml
         setContentView(R.layout.activity_welcome);
-
         // Retrieve current user from Parse.com
-        ParseUser currentUser = ParseUser.getCurrentUser();
+        currentUser = ParseUser.getCurrentUser();
+        totalSteps = currentUser.getNumber(TOTAL_STEPS).intValue();
 
         // Convert currentUser into String
         String struser = currentUser.getUsername().toString();
@@ -43,5 +55,44 @@ public class WelcomeActivity extends Activity {
                 finish();
             }
         });
+
+        count = (TextView) findViewById(R.id.step_count);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        activityRunning = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        activityRunning = false;
+        // if you unregister the last listener, the hardware will stop detecting step events
+//        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (activityRunning) {
+            totalSteps = (int) sensorEvent.values[0];
+            count.setText(String.valueOf(totalSteps) + "steps");
+            currentUser.put(TOTAL_STEPS,totalSteps);
+            currentUser.saveInBackground();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
