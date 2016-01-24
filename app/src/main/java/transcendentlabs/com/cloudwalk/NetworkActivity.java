a6cd4f8c4adc065fc5dd69b0287d696927f9a241
Allen Gao <allen001@live.ca>
2016-01-23 22:13:23 -0500

    Created a way to programmatically enable the mobile hotspot (currently triggered by a button)

--- app/src/main/java/transcendentlabs/com/cloudwalk/NetworkActivity.java
+++ app/src/main/java/transcendentlabs/com/cloudwalk/NetworkActivity.java
@@ -2,7 +2,9 @@ package transcendentlabs.com.cloudwalk;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Network;
 import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
@@ -10,12 +12,17 @@ import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
 import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.parse.ParseUser;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -39,6 +46,21 @@ public class NetworkActivity extends AppCompatActivity{
         ActionBar bar = getSupportActionBar();
         Window window = getWindow();
         Util.setActionBarColour(bar, window, this);
+
+        Button hotspot = (Button) findViewById(R.id.hotspot);
+
+        hotspot.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                // Logout current user
+                if (!Settings.System.canWrite(NetworkActivity.this)) {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                    startActivity(intent);
+                }
+                Hotspot.configApState(NetworkActivity.this);
+
+            }
+        });
 
         mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         mChannel = mManager.initialize(this, getMainLooper(), null);
